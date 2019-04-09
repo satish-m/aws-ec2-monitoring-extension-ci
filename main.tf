22c661c54b4185a461baee89bf6b8d9399be54d3
satish.muddam <satish.muddam@appdynamics.com>
2019-04-09 14:26:08 -0700

    Creating CI pipeline

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
 
 provider "aws" {
-
+   access_key = "${var.AWS_ACCESS_KEY_ID}"
+   secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+   region = "us-east-1"
 }
 
 resource "aws_instance" "aws_btd" {
